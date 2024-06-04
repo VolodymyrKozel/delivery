7628f34113b81e627593fc1336ff2dcd913498e1
Volodymyr <Volodumurvel@gmail.com>
2024-06-04 21:31:18 +0200

    add tab  navigation

--- App.js
+++ App.js
@@ -1,8 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import RestaurantsScreen from "./src/features/restaurants/screens/RestaurantsScreen";
+import SettingsScreen from "./src/features/settings/screens/SettingsScreen";
+import MapScreens from "./src/features/map/screens/MapScreens";
 import { ThemeProvider } from "styled-components/native";
 import { theme } from "./src/infrastructure/theme";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
 
 import {
   useFonts as useOswald,
@@ -25,7 +31,13 @@ export default function App() {
   return (
     <>
       <ThemeProvider theme={theme}>
-        <RestaurantsScreen />
+        <NavigationContainer>
+          <Tab.Navigator>
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={MapScreens} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </NavigationContainer>
       </ThemeProvider>
       <StatusBar style="auto" />
     </>
